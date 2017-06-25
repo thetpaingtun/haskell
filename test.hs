2a84fd71c34632b7f75f434967832dbaab633b72
Thet Paing Tun <thetpaingtun93@gmail.com>
2017-06-25 21:29:55 +0630

    update

--- test.hs
+++ test.hs
@@ -6,5 +6,20 @@ import System.IO
 int = 3
 
 
-fact 0 = 1
-fact n = n * fact n-1
+--fact 0 = 1
+--fact n = n * fact (n-1)
+
+
+double x = x + x
+
+
+
+quad x = double (double x)
+
+
+fact n = product[1..n]
+
+
+avg ns = sum ns `div` length ns
+
+
