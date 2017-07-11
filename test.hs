a477eceab842e79f90e4b7d4d5c72e76a1f55038
Thet Paing Tun <thetpaingtun93@gmail.com>
2017-07-11 17:46:25 +0630

    update

--- test.hs
+++ test.hs
@@ -44,6 +44,59 @@ lucky x = "Sorry"
 
 
 
+charName :: Char -> String
+charName 'a' = "Albert"
+charName 'b' = "Bob"
+charName x = "Invalid char"
+
+
+addVectors :: Num a => (a,a) -> (a,a) -> (a,a)
+addVectors (x1,y1) (x2,y2) = (x1+x2,y1+y2)
+
+
+first :: (a,b,c) -> a
+first (a,_,_) = a
+
+second :: (a,b,c)-> b
+second (_,b,_) = b
+
+third :: (a,b,c) -> c
+third (_,_,c) = c
+
+head' :: [a] -> a
+head' [] = error "list is empty"
+head' (x:_) = x
+
+tell :: Show a => [a] -> String
+tell [] = "The list is empty"
+tell (x:[]) = "The list has one element "++ show x
+tell (x:y:[]) = "The list has two elements "++ show x ++ " and "++ show y
+tell (x:y:_) = "The list is long and the first two elements are "++ show x ++ " and " ++ show y
+
+
+length' :: Num b => [a] -> b
+length' [] = 0
+length' (_:xs) = 1 + length' xs 
+
+
+sum' :: Num a => [a] -> a
+sum'[] = 0
+sum' (x:xs) = x + sum' xs
+
+capital :: String -> String 
+capital "" = "empty string" 
+capital all@(x:xs) = "The first letter of "++ all ++ " is " ++ [x]
+
+
+
+
+
+
+
+
+
+
+
 
 
 
