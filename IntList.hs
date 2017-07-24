1d0e114e5aebd5bd7b612c6b29cdc45b47da467c
Thet Paing Tun <thetpaingtun93@gmail.com>
2017-07-24 17:20:55 +0630

    update

--- IntList.hs
+++ IntList.hs
@@ -62,9 +62,32 @@ conc (Cons x Empty) ys = (Cons x ys)
 conc (Cons x xs) ys    = (Cons x (conc xs ys))
 
 
+--there exit some bugs
 qSort :: IntList -> IntList 
 qSort Empty = error "Empty List"
 qSort (Cons x Empty) = (Cons x Empty)
 qSort (Cons x xs)    = (conc (conc smaller (Cons x Empty)) larger)
                         where smaller = filter' (<=x) xs
                               larger  = filter' (>x) xs
+
+length' :: IntList -> Int
+length' Empty = 0
+length' (Cons x xs) = 1 + length' xs
+
+any' :: (Int->Bool) -> IntList -> Bool
+any' _ Empty = False
+any' f (Cons x xs) 
+   | f x       = True
+   | otherwise = any' f xs
+
+
+
+all' :: (Int->Bool) -> IntList -> Bool
+all' _ Empty =  True
+all' f (Cons x xs) 
+   | (f x)       = all' f xs
+   | otherwise   = False
+
+
+
+
